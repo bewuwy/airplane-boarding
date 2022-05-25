c51095f85590e9eddf32bc78d0a43238e0dfb732
bewuwy <bewuwy@gmail.com>
2022-05-25 18:22:26 +0200

    cleaned the code

--- gui.py
+++ gui.py
@@ -11,19 +11,12 @@ config = configparser.RawConfigParser()
 configFilePath = r'config.txt'
 config.read(configFilePath)
 
-if os.name == 'nt':
-    import ctypes
-
-    user32 = ctypes.windll.user32
-    screensize = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
-else:
-    screensize = (int(config.get("window", "width")), int(config.get("window", "height")))
-
 # CONSTANTS:
-SCREENSIZE = WIDTH, HEIGHT = screensize  # 1920, 1080  # 1024, 768
+SCREENSIZE = WIDTH, HEIGHT = int(config.get("window", "width")), int(config.get("window", "height"))
 BLACK = (0, 0, 0)
 GREY = (160, 160, 160)
 PADDING = PADTOPBOTTOM, PADLEFTRIGHT = 60, 60
+
 # VARS:
 _VARS = {'surf': False, "auto": False, "auto_time": 1000, "end": False}
 turns = {}
@@ -43,9 +36,10 @@ def main():
 
     print(config.get("passengers", "type"))
 
-    m, n = int(config.get("aeroplane", "width")), int(config.get("aeroplane", "height"))  # 40, 13
+    m, n = int(config.get("aeroplane", "seats")), int(config.get("aeroplane", "rows"))
     corridor = config.get("aeroplane", "corridor") or n // 2
     corridor = int(corridor)
+    
     _VARS["pt"] = passengersType = config.get("passengers", "type") or "random"
     _VARS["p_opts"] = {}
     _VARS["p_opts"]["section_width"] = int(config.get("sections", "width"))
@@ -54,88 +48,96 @@ def main():
     _VARS["t_opts"] = {}
     _VARS["t_opts"]["barging_time"] = int(config.get("passengers", "bargingTime"))
 
-    _VARS["dims"] = (m, n, corridor)
-    _VARS["t"] = next_((0, plane.getGrid(m, n), plane.getPassengers(m, n, corridor, passengersType, _VARS["p_opts"])))
-
+    # _VARS["dims"] = (m, n, corridor)
+    # _VARS["t"] = next_((0, plane.getGrid(m, n), plane.getPassengers(m, n, corridor, passengersType, _VARS["p_opts"])))
+    
+    _VARS["plane"] = plane.Plane(m, n, corridor)
+    _VARS["plane"].getPassengers(_VARS["pt"])
+    
     while True:
-        # drawGrid(3, 7)
         checkEvents()
         _VARS['surf'].fill(GREY)
 
         if _VARS['auto'] and _VARS["auto_time"] > 0:
-            drawText(str(round(1 / (_VARS["auto_time"] / 1000), 1)) + " turns/s", WIDTH - 2.5 * PADDING[0],
-                     HEIGHT - (PADDING[1] / 2))
+            drawText(str(round(1 / (_VARS["auto_time"] / 1000), 1)) + " turns/s", 
+                     WIDTH - 2.5 * PADDING[0], HEIGHT - (PADDING[1] / 2))
 
         # pygame.display.update()
 
 
-def prev_():
-    if len(turns) < 3 or _VARS["t"][0] < 2:
-        return
+# def prev_():
+#     if len(turns) < 3 or _VARS["t"][0] < 2:
+#         return
+# 
+#     _VARS["auto"] = False
+# 
+#     print("prev to", turns[1][0])
+#     if not _VARS["end"] and 1 in turns:
+#         print(turns[1])
+#         next_(turns[1])  # _VARS['t'][0]-2
+# 
+#     return
 
-    _VARS["auto"] = False
 
-    print("prev to", turns[1][0])
-    if not _VARS["end"] and 1 in turns:
-        print(turns[1])
-        next_(turns[1])  # _VARS['t'][0]-2
-
-    return
-
-
-def next_(t):
-    if len(t[2]) == 0:
+def next_():
+    plane = _VARS["plane"]
+    
+    if len(plane.passengers) == 0:
         _VARS['auto'] = False
         _VARS['end'] = True
         return
+    
+    m, n, cy = plane.m, plane.n, plane.corridorSeat
+    # m, n, corridor = _VARS["dims"]
 
-    m, n, corridor = _VARS["dims"]
-
-    drawTurnNumber(t[0])
+    drawTurnNumber(plane.turn)
     drawText("Type: " + _VARS["pt"], WIDTH - 3 * PADDING[0], (PADDING[1] / 4))
 
-    t = plane.next_turn(t[2], t[0], t[1], corridor, _VARS["t_opts"])
+    t = plane.next_turn(_VARS["t_opts"])
 
-    drawGrid(m, n)
-    for i in range(m):
-        drawRect(i, corridor, (m, n))
+    drawGrid(n, m)
+    for i in range(n):  # draw corridor in black
+        drawRect(cy, i)
 
-    for i in range(len(t[1])):
-        for j in range(len(t[1][i])):
-            s = plane.checkPlaceStatus(t[1], i, j, corridor)
+    for seat in range(len(plane.grid)):
+        for row in range(len(plane.grid[seat])):
+            status = plane.checkPlaceStatus(row, seat)
 
-            if s[0] == "occupied":
-                drawRect(i, j, (m, n), (69, 179, 224))  # blue
-                for p in s[1]:
+            if status[0] == "standing":  # drawing a standing person
+                drawRect(seat, row, (69, 179, 224))  # blue
+                for p in status[1]:
                     if p.toWait > 0:
-                        drawRect(i, j, (m, n), (171, 209, 0))  # yellow
-                        drawTextGrid(i, j + 0.2, (m, n), f"{p.toWait} left")
+                        drawRect(seat, row, (171, 209, 0))  # yellow
+                        drawTextGrid(seat + 0.2, row, f"{p.toWait} left")
 
                         break
 
-                for p in range(len(s[1])):
-                    drawTextGrid(i, j + p * 0.4, (m, n), s[1][p].ticket())
-            elif s[0] == 'packing':
-                drawRect(i, j, (m, n), (201, 29, 62))  # red
-                drawTextGrid(i, j, (m, n), s[1].ticket())
-                drawTextGrid(i, j + 0.2, (m, n), f"{s[1].seatingTime} left")
-            elif s[0] == "full":
-                drawRect(i, j, (m, n), (71, 209, 71))  # green
-                drawTextGrid(i, j, (m, n), s[1].ticket())
-                drawTextGrid(i, j + 0.2, (m, n), s[1].idleTime)
+                for p in range(len(status[1])):
+                    drawTextGrid(seat + p * 0.4, row, status[1][p].ticket())
+                    
+            elif status[0] == 'packing':  # drawing a person packing
+                drawRect(seat, row, (201, 29, 62))  # red
+                drawTextGrid(seat, row, status[1].ticket())
+                drawTextGrid(seat + 0.2, row, f"{status[1].seatingTime} left")
+                
+            elif status[0] == "boarded":  # drawing a person that's already booked
+                drawRect(seat, row, (71, 209, 71))  # green
+                drawTextGrid(seat, row, status[1].ticket())
+                drawTextGrid(seat + 0.2, row, status[1].idleTime)
 
     pygame.display.update()
 
-    if t[0] not in turns:
-        turns[t[0]] = t
-    return t
-
 
 def reset():
-    _VARS["t"] = next_((0, plane.getGrid(_VARS["dims"][0], _VARS["dims"][1]), plane.getPassengers(_VARS["dims"][0],
-                        _VARS["dims"][1], _VARS["dims"][2], _VARS["pt"], _VARS["p_opts"])))
+    # _VARS["t"] = next_((0, plane.getGrid(_VARS["dims"][0], _VARS["dims"][1]), plane.getPassengers(_VARS["dims"][0],
+    #                    _VARS["dims"][1], _VARS["dims"][2], _VARS["pt"], _VARS["p_opts"])))
+    
+    _VARS["plane"] = plane.Plane(_VARS["plane"].m, _VARS["plane"].n, _VARS["plane"].corridorSeat)
+    _VARS["plane"].getPassengers(_VARS["pt"])
+    
     _VARS["end"] = False
-
+    next_()
+    
 
 def drawTurnNumber(turn):
     t = font.render("Turn: " + str(turn), False, BLACK)
@@ -147,21 +149,21 @@ def drawText(text, x, y, color=BLACK):
     _VARS['surf'].blit(t, (x, y))
 
 
-def drawTextGrid(i, j, grid, text, color=BLACK):
-    horizontal_cellsize = (WIDTH - (PADLEFTRIGHT * 2)) / grid[0]
-    vertical_cellsize = (HEIGHT - (PADTOPBOTTOM * 2)) / grid[1]
+def drawTextGrid(m, n, text, color=BLACK):
+    horizontal_cellsize = (WIDTH - (PADLEFTRIGHT * 2)) / _VARS["plane"].n
+    vertical_cellsize = (HEIGHT - (PADTOPBOTTOM * 2)) / _VARS["plane"].m
 
     t = font.render(str(text), False, color)
-    _VARS['surf'].blit(t, (PADDING[0] + i * horizontal_cellsize, PADDING[0] + j * vertical_cellsize))
+    _VARS['surf'].blit(t, (PADDING[0] + n * horizontal_cellsize, PADDING[0] + m * vertical_cellsize))
 
 
-def drawRect(i, j, grid, color=BLACK):
-    horizontal_cellsize = (WIDTH - (PADLEFTRIGHT * 2)) / grid[0]
-    vertical_cellsize = (HEIGHT - (PADTOPBOTTOM * 2)) / grid[1]
+def drawRect(m, n, color=BLACK):
+    horizontal_cellsize = (WIDTH - (PADLEFTRIGHT * 2)) / _VARS["plane"].n
+    vertical_cellsize = (HEIGHT - (PADTOPBOTTOM * 2)) / _VARS["plane"].m
 
     pygame.draw.rect(
         _VARS['surf'], color,
-        (PADDING[0] + i * horizontal_cellsize, PADDING[0] + j * vertical_cellsize, horizontal_cellsize,
+        (PADDING[0] + n * horizontal_cellsize, PADDING[0] + m * vertical_cellsize, horizontal_cellsize,
          vertical_cellsize)
     )
 
@@ -216,7 +218,7 @@ def checkEvents():
     for event in pygame.event.get():
         if event.type == pygame.USEREVENT:
             if not _VARS["end"] and _VARS["auto"] and pygame.time.get_ticks() - _VARS["last_auto"] > _VARS["auto_time"]:
-                _VARS["t"] = next_(_VARS["t"])
+                next_()
                 _VARS["last_auto"] = pygame.time.get_ticks()
 
         if event.type == pygame.QUIT:
@@ -231,7 +233,7 @@ def checkEvents():
 
             elif event.key == K_RIGHT:
                 if not _VARS["end"]:
-                    _VARS["t"] = next_(_VARS["t"])
+                    next_()
             elif event.key == K_SPACE:
                 print("SPACE")
 
--- plane.py
+++ plane.py
@@ -47,191 +47,204 @@ class Person:
             return f"{self.ticketRow+1}{chr(65 + self.ticketSeat//26 - 1)}{chr(65 + self.ticketSeat - 26)}"
 
 
-def getPassengers(a, b, cy, type_, opts=None):  # generate random people
-    pt = None
-    if opts is None:
-        opts = {}
-    elif "packing_time" in opts:
-        pt = opts["packing_time"]
+class Plane:
+    def __init__(self, m, n, corridor_y):
+        self.grid = []
+        self.corridorSeat = corridor_y
+        self.passengers = []
+        
+        self.m = m
+        self.n = n
+        
+        self.turn = 0
+        
+        for seat in range(m):
+            t_ = []
+            for row in range(n):
+                t_.append([])
+            self.grid.append(t_)
+            
 
-    p = []
-    if type_ == "random":
-        for i in range(b):
-            if i != cy:
-                for j in range(a):
-                    p.insert(randrange(len(p) + 1), Person(j, i, str(j) + str(i), pt))
-    elif type_ == "seat":
-        for i in range(b):
-            if i != cy:
-                i_ = list(range(0, a))
-                random.shuffle(i_)
-
-                for j in range(a):
-                    if i > cy:
-                        p.append(Person(i_[j], b-i+cy, str(j) + str(i), pt))
-                    else:
-                        p.append(Person(i_[j], i, str(j) + str(i), pt))
-    elif type_ == "section":
+    def getPassengers(self, type_, options=None):  # generate random people
+        packing_time = None
         section_width = 6
-        if "section_width" in opts:
-            section_width = opts["section_width"]
+        
+        if options is None:
+            options = {}
+        if "packing_time" in options:
+            packing_time = options["packing_time"]
+        if "section_width" in options:
+            section_width = options["section_width"]
 
-        def chunks(lst, n_):
-            """Yield successive n-sized chunks from lst."""
-            for i__ in range(0, len(lst), n_):
-                yield lst[i__:i__ + n_]
+        p = []  # passengers list
+        if type_ == "random":  # random passengers distribution 
+            for seat in range(self.m):
+                if seat != self.corridorSeat:
+                    for row in range(self.n):
+                        p.insert(randrange(len(p) + 1), 
+                                 Person(row, seat, str(row) + str(seat), packing_time))
+                        
+        elif type_ == "seat":  # seat-based passengers distribution
+            for seat in range(self.m):
+                if seat != self.corridorSeat:
+                    i_ = list(range(0, self.n))
+                    random.shuffle(i_)
 
-        sections = list(chunks(list(range(0, a)), section_width))
-        sections.reverse()
+                    for row in range(self.n):
+                        if seat > self.corridorSeat:
+                            p.append(Person(i_[row], self.m - seat + self.corridorSeat, str(row) + str(seat), packing_time))
+                        else:
+                            p.append(Person(i_[row], seat, str(row) + str(seat), packing_time))
+                            
+        elif type_ == "section":  # section-based passengers distribution
+            def chunks(lst, n_):
+                """Yield successive n-sized chunks from lst."""
+                for i__ in range(0, len(lst), n_):
+                    yield lst[i__:i__ + n_]
 
-        for s in sections:
-            sp = []
-            for column in range(len(s)):
-                for row in range(b):
-                    if row != cy:
-                        # print(Person(s[column], row, str(s[column]) + str(row)))
-                        sp.insert(randrange(len(sp) + 1), Person(s[column], row, str(s[column]) + str(row)))  # a-i-1
+            sections = list(chunks(list(range(0, self.n)), section_width))
+            sections.reverse()
 
-            p.extend(sp)
+            for s in sections:
+                sp = []  # section passengers
+                for row in range(len(s)):
+                    for seat in range(self.m):
+                        if seat != self.corridorSeat:
+                            # print(Person(s[column], row, str(s[column]) + str(row)))
+                            sp.insert(randrange(len(sp) + 1), Person(s[row], seat, str(s[row]) + str(seat)))  # a-i-1
 
-    return p
+                p.extend(sp)
+
+        self.passengers = p
+
+            
+    def checkIfPlaceEmpty(self, row, seat):
+        if len(self.grid[seat][row]) == 0 or \
+            (len(self.grid[seat][row]) == 1 and self.grid[seat][row][0].check_seated()):
+            
+            return True
+        else:
+            return False
+        
+    
+    def checkPlaceStatus(self, row, seat):
+        if len(self.grid[seat][row]) == 0:
+            return "empty", None
+        if len(self.grid[seat][row]) == 1:
+            if self.grid[seat][row][0].check_seated():
+                return "boarded", self.grid[seat][row][0]
+            elif row == self.grid[seat][row][0].ticketRow and seat == self.corridorSeat:
+                return "packing", self.grid[seat][row][0]
+
+        return "standing", self.grid[seat][row]
 
 
-def getGrid(a, b):
-    g = []  # airplane a by b
-    for m_ in range(a):  # populate the grid
-        t_ = []
-        for n_ in range(b):
-            t_.append([])
-        g.append(t_)
-
-    return g
-
-
-def checkIfPlaceEmpty(grid_, row, seat):
-    if len(grid_[row][seat]) == 0 or (len(grid_[row][seat]) == 1 and grid_[row][seat][0].check_seated()):
+    def checkIfAllSeated(self):
+        for seat in self.grid:
+            for place in seat:
+                if place[0].check_seated() == False:
+                    return False
         return True
-    else:
-        return False
 
 
-def checkIfAllSeated(grid_):
-    for row in grid_:
-        for seat in row:
-            if seat[0].check_seated() is False:
-                return False
-    return True
+    def movePerson(self, passenger, row, seat):
+        if passenger.toWait > 0:
+            passenger.toWait -= 1
+            passenger.idleTime += 1
+
+            return False
+
+        # deleting the person from old place
+        if passenger.currentRow > -1 and passenger.currentSeat > -1 \
+        and passenger in self.grid[passenger.currentSeat][passenger.currentRow]:
+            
+            self.grid[passenger.currentSeat][passenger.currentRow].remove(passenger)
+
+        # adding the person in new place
+        passenger.currentSeat = seat
+        passenger.currentRow = row
+        
+        self.grid[seat][row].append(passenger)
+
+        return True
 
 
-def checkPlaceStatus(grid_, row, seat, corridor):
-    if len(grid_[row][seat]) == 0:
-        return "empty", None
-    if len(grid_[row][seat]) == 1:
-        if grid_[row][seat][0].check_seated():
-            return "full", grid_[row][seat][0]
-        elif row == grid_[row][seat][0].ticketRow and seat == corridor:
-            return "packing", grid_[row][seat][0]
+    def next_turn(self, options=None):
+        barging_time = 1
+        
+        if options is None:
+            options = {}
+        if "barging_time" in options:
+            barging_time = options["barging_time"]
 
-    return "occupied", grid_[row][seat]
+        # print(f"turn {t_num}")
 
-
-def movePerson(p_, row, seat, grid_):
-    if p_.toWait > 0:
-        p_.toWait -= 1
-        p_.idleTime += 1
-
-        return grid_
-
-    # deleting the person from old place
-    if p_.currentRow > -1 and p_.currentSeat > -1 and p_ in grid_[p_.currentRow][p_.currentSeat]:
-        grid_[p_.currentRow][p_.currentSeat].remove(p_)
-
-    # adding the person in new place
-    p_.currentRow = row
-    p_.currentSeat = seat
-
-    grid_[row][seat].append(p_)
-
-    return grid_
-
-
-def next_turn(people_, t_num, grid_, corridor, opts=None):
-    if opts is None:
-        opts = {}
-
-    barging_time = 1
-    if "barging_time" in opts:
-        barging_time = opts["barging_time"]
-
-    toRemove = []
-    # print(f"turn {t_num}")
-
-    for p in people_:
-        if p.currentRow < 0:  # person not on plane yet
-            if checkIfPlaceEmpty(grid_, 0, corridor):  # entrance empty
-                grid_ = movePerson(p, 0, corridor, grid_)
-                continue
-            else:  # entrance taken
-                p.idleTime += 1
-                continue
-
-        if p.currentRow >= 0:  # person already on plane
-            if p.currentRow == p.ticketRow:  # person in a correct row
-                if p.currentSeat == p.ticketSeat:  # person in a correct seat
-                    toRemove.append(p)
+        toRemove = []
+        for p in self.passengers:  # trying to move every passenger
+            if p.currentRow < 0:  # person not on plane yet
+                if self.checkIfPlaceEmpty(0, self.corridorSeat):  # entrance empty
+                    self.movePerson(p, 0, self.corridorSeat)
                     continue
-
-                if p.ticketSeat < corridor:
-                    toMoveY = p.currentSeat - 1
-                else:
-                    toMoveY = p.currentSeat + 1
-
-                if p.currentSeat == corridor and p.seatingTime > 0:  # if person in the corridor and still has
-                    # something to pack
-                    p.seatingTime -= 1
-                    continue
-                elif checkIfPlaceEmpty(grid_, p.currentRow, toMoveY):  # if seat left/right of current seat empty
-                    grid_ = movePerson(p, p.currentRow, toMoveY, grid_)
-
-                    if len(grid_[p.currentRow][toMoveY]) > 1:
-                        p.toWait = barging_time
-
-                    continue
-                else:  # if seat left/right of current seat taken
+                else:  # entrance taken
                     p.idleTime += 1
-
-            elif p.currentRow != p.ticketRow:  # person not in a correct row
-                if checkIfPlaceEmpty(grid_, p.currentRow + 1, p.currentSeat):  # if next place in the corridor empty
-                    grid_ = movePerson(p, p.currentRow + 1, p.currentSeat, grid_)
                     continue
-                else:  # standing idle
-                    p.idleTime += 1
 
-    for i_ in toRemove:
-        people_.remove(i_)
-        # grid_[i_.currentRow][i_.currentSeat] = 0
+            if p.currentRow >= 0:  # person already on plane
+                if p.currentRow == p.ticketRow:  # person in a correct row
+                    if p.currentSeat == p.ticketSeat:  # person in a correct seat
+                        toRemove.append(p)
+                        continue
 
-    t_num += 1  # increment number of turns
-    # pp.pprint(grid_)
+                    if p.ticketSeat < self.corridorSeat:
+                        toMoveY = p.currentSeat - 1
+                    else:
+                        toMoveY = p.currentSeat + 1
 
-    return t_num, grid_, people_
+                    if p.currentSeat == self.corridorSeat and p.seatingTime > 0:  
+                        # if person in the corridor and still has something to pack
+                        p.seatingTime -= 1
+                        continue
+                    elif self.checkIfPlaceEmpty(p.currentRow, toMoveY):  # if seat left/right of current seat empty
+                        self.movePerson(p, p.currentRow, toMoveY)
+
+                        if len(self.grid[toMoveY][p.currentRow]) > 1:
+                            p.toWait = barging_time
+
+                        continue
+                    else:  # if seat left/right of current seat taken
+                        p.idleTime += 1
+
+                elif p.currentRow != p.ticketRow:  # person not in a correct row
+                    if self.checkIfPlaceEmpty(p.currentRow + 1, p.currentSeat):  # if next place in the corridor empty
+                        self.movePerson(p, p.currentRow + 1, p.currentSeat)
+                        continue
+                    else:  # standing idle
+                        p.idleTime += 1
+
+        for p_ in toRemove:
+            self.passengers.remove(p_)
+            # grid_[i_.currentRow][i_.currentSeat] = 0
+
+        self.turn += 1  # increment number of turns
+        # pp.pprint(grid_)
+
+        return self.turn  # t_num, grid_, people_
 
 
 if __name__ == '__main__':
-    m, n = 8 + 1, 43  # +1 in m because of corridor
-    corridorY = 4
-    types = ["random", "seat", ["section", {"section_width": 6}], ["section", {"section_width": 3}],
-             ["section", {"section_width": 12}]]
-
-    # types = [["random", {"barging_time": 1, "packing_time": 2}], ["random", {"barging_time": 0, "packing_time": 4}]]
-
-    s_opt = {"section_width": 6}
-    tests = 100
+    m, n, cy = 6 + 1, 33, 3  # +1 in m because of corridor
     
-    print(f"plane with {n} rows and {m} seats, corridor at {corridorY}")
+    # test_types = ["random"]
+    test_types = ["random", "seat", ["section", {"section_width": 6}], ["section", {"section_width": 3}],
+             ["section", {"section_width": 12}]]
+    # test_types = [["random", {"barging_time": 1, "packing_time": 2}], ["random", {"barging_time": 0, "packing_time": 4}]]
+
+    tests = 10
+    
+    print(f"plane with {n} rows and {m} seats, corridor at {cy}")
 
     all_results = {}
-    for t in types:
+    for t in test_types:
         print()
         results = []
         t_opts = {}
@@ -253,12 +266,14 @@ if __name__ == '__main__':
         for i in range(tests):
             print(f"{i+1}/{tests} {t}", end='\r')
 
-            people = getPassengers(n, m, corridorY, t, s_opt)
-            t_num = 0
-            grid = getGrid(n, m)
-
-            while people:
-                t_num, grid, people = next_turn(people, t_num, grid, corridorY, t_opts)
+            # people = getPassengers(n, m, corridorY, t, s_opt)
+            # t_num = 0
+            # grid = getGrid(n, m)
+            plane = Plane(m, n, cy)
+            plane.getPassengers(t, s_opt)
+            
+            while plane.passengers:
+                t_num = plane.next_turn(t_opts)
 
             # print(t_num, end='\r')
             results.append(t_num)
